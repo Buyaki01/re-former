ebee2a04b29751913c8858c0a1a62d585d565ec6
Bluette1 <marylene.sawyer@gmail.com>
2020-07-21 09:40:19 +0200

    Add lint config

--- db/migrate/20200720123134_create_users.rb
+++ db/migrate/20200720123134_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username, presence: true, limit: 20, uniqueness: true, length: { minimum: 2 }
       t.string :email, presence: true, limit: 20, uniqueness: true, length: { minimum: 10 }
-      t.string :password, presence: true, limit: 8, length: { minimum: 8}
+      t.string :password, presence: true, limit: 8, length: { minimum: 8 }
 
       t.timestamps
     end
